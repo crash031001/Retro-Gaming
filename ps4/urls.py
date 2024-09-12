b0e4e65c6f43f442d3e12dd30c28b1a059956b59
crash031001 <106040085+crash031001@users.noreply.github.com>
2024-09-12 10:57:59 -0400

    se colecionaron los static

--- ps4/urls.py
+++ ps4/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import HomeView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('retro_gaming/',include('retro_gaming.urls',namespace='retro_gaming')),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
